ee37f5036bd5f8cecab7d18e937dab677adc4719
wufangjun <wufangjun@aplum.onaliyun.com>
2023-05-31 19:57:47 +0800

    minor

--- utils/utils.go
+++ utils/utils.go
@@ -23,16 +23,18 @@ func Round(x float64, n int) float64 {
 	return math.Round(x*math.Pow10(n)) / math.Pow10(n)
 }
 
+// 两个floa64的数是否在误差范围内相等，
 func IsEqual(x, y float64, precision float64) bool {
 	return math.Dim(x, y) < precision
 }
 
+// 随机返回数组中的某值
 func RandArray(arr ...interface{}) interface{} {
 	rand.Seed(time.Now().UnixNano())
 	return arr[rand.Intn(len(arr))]
 }
 
-// 要求 a < b
+// 获取[a,b)范围内的随机整数，要求 a < b
 func RandInterval(a, b int64) int64 {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Int63n(b-a+1) + a
